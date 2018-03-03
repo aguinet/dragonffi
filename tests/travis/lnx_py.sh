4c053da076e2e53384a190e9a9e258f41d099e3a
Adrien Guinet <adrien@guinet.me>
2018-03-03 21:39:56 +0100

    Travis for python 3.4 bindings under Linux

--- tests/travis/lnx_py.sh
+++ tests/travis/lnx_py.sh
@@ -4,8 +4,12 @@
 DIR=$(dirname $(readlink -f $0))
 . "$DIR/common.sh"
 
+if [ ! -z ${PYTHON_EXECUTABLE+x} ]; then
+  PYTHON_EXECUTABLE="-DPYTHON_EXECUTABLE=$(which $PYTHON_EXECUTABLE)"
+fi
+
 mkdir build
 cd build
-cmake -DPYTHON_BINDINGS=ON -DPYTHON_VERSION=$PYTHON_VERSION -DBUILD_TESTS=OFF -DLLVM_CONFIG=../$LLVM/bin/llvm-config -DCMAKE_BUILD_TYPE=debug -G Ninja ..
+cmake -DPYTHON_BINDINGS=ON -DPYTHON_VERSION=$PYTHON_VERSION -DBUILD_TESTS=OFF -DLLVM_CONFIG=../$LLVM/bin/llvm-config -DCMAKE_BUILD_TYPE=debug $PYTHON_EXECUTABLE -G Ninja ..
 ninja
 ninja check
