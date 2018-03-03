061e2bbaac705ef3725ce3c55de949cb5603eeb4
Adrien Guinet <adrien@guinet.me>
2018-03-03 20:31:42 +0100

    Travis for python 3.6 bindings under OSX

--- tests/travis/osx_setup.sh
+++ tests/travis/osx_setup.sh
@@ -8,3 +8,6 @@ get_llvm osx c34f4d9c5634cf93e43ee97ac322702722b5100dfc1c6d7b0a0afb73c6bb5537
 configure_pip
 
 brew install ccache ninja
+if [ "${PYTHON_VERSION:0:1}" == "3" ]; then
+  brew upgrade python3
+fi
