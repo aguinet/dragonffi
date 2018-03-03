e6ddce52761282902c50d8b71d5755d54e575fb6
Adrien Guinet <adrien@guinet.me>
2018-03-03 20:17:27 +0100

    Travis for python 2.7 bindings under OSX

--- tests/travis/osx.sh
+++ tests/travis/osx.sh
@@ -7,6 +7,6 @@ DIR=$(dirname $(realpath $0))
 mkdir build
 cd build
 export PATH=$PATH:$HOME/Library/Python/2.7/bin/
-cmake -DPYTHON_BINDINGS=$PYTHON_BINDINGS -DLLVM_CONFIG=../llvm-5.0.1.dffi/bin/llvm-config -DCMAKE_BUILD_TYPE=debug -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -G Ninja ..
+cmake -DPYTHON_BINDINGS=$PYTHON_BINDINGS -DBUILD_TESTS=$BUILD_TESTS -DLLVM_CONFIG=../llvm-5.0.1.dffi/bin/llvm-config -DCMAKE_BUILD_TYPE=debug -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -G Ninja .. -DPYTHON_VERSION=$PYTHON_VERSION
 ninja
 ninja check
