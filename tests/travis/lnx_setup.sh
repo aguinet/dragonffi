5a7e1ff3d1ab2a52545d3952631f3a5f49010e29
Adrien Guinet <adrien@guinet.me>
2018-03-16 21:12:43 +0100

    wip

--- tests/travis/lnx_setup.sh
+++ tests/travis/lnx_setup.sh
@@ -8,6 +8,15 @@ get_llvm lnx$ARCH 291ae264965240493f0d5f2e154ab7dbfcf2c7de23291ba415822594ca6cfe
 configure_pip
 
 if [ ! -z $ARCH ]; then
-  sudo dpkg-architecture --add $ARCH
+  case $ARCH in
+    aarch64)
+      DEB_ARCH=arm64
+      ;;
+    *)
+      DEB_ARCH=$ARCH
+      ;;
+  esac
+  sudo dpkg-architecture --add $DEB_ARCH
   sudo apt-get update
+  sudo apt-get install libstdc++-dev:$DEB_ARCH libxml2-dev:$DEB_ARCH binutils-$ARCH-linux-gnu
 fi
