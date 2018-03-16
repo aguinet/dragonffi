5baddd77b28ef3dee2d3880d458341f8c9b7ec32
Adrien Guinet <adrien@guinet.me>
2018-03-16 21:04:09 +0100

    Travis support for Linux/aarch64

--- tests/travis/lnx.sh
+++ tests/travis/lnx.sh
@@ -4,8 +4,12 @@
 DIR=$(dirname $(readlink -f $0))
 . "$DIR/common.sh"
 
+if [ ! -z $ARCH ]; then
+  ARCH="x86_64"
+fi
+
 mkdir build
 cd build
-cmake -DPYTHON_BINDINGS=OFF -DLLVM_CONFIG=../$LLVM/bin/llvm-config -DCMAKE_BUILD_TYPE=debug -G Ninja ..
+cmake -DPYTHON_BINDINGS=OFF -DLLVM_CONFIG=../$LLVM/bin/llvm-config -DCMAKE_BUILD_TYPE=debug -G Ninja -DCMAKE_C_FLAGS="-target $ARCH-linux-gnu" -DCMAKE_CXX_FLAGS="-target $ARCH-linux-gnu" -DCMAKE_C_COMPILER="clang-6.0" -DCMAKE_CXX_COMPILER="clang++-6.0" ..
 ninja
 ninja check
--- tests/travis/lnx_setup.sh
+++ tests/travis/lnx_setup.sh
@@ -4,5 +4,10 @@
 DIR=$(dirname $(readlink -f $0))
 . "$DIR/common.sh"
 
-get_llvm lnx 291ae264965240493f0d5f2e154ab7dbfcf2c7de23291ba415822594ca6cfecb
+get_llvm lnx$ARCH 291ae264965240493f0d5f2e154ab7dbfcf2c7de23291ba415822594ca6cfecb
 configure_pip
+
+if [ ! -z $ARCH ]; then
+  sudo dpkg-architecture --add $ARCH
+  sudo apt-get update
+fi
