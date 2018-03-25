1d59e3a1501e8bf20322089e8ee3b005297c8dc2
Adrien Guinet <adrien@guinet.me>
2018-03-25 17:31:16 +0200

    WIP

--- tests/travis/lnx_docker_setup.sh
+++ tests/travis/lnx_docker_setup.sh
@@ -6,7 +6,7 @@ DIR=$(dirname $(readlink -f $0))
 
 case $ARCH in
   aarch64)
-    LLVM_HASH=d2a9ecc958857655360823cf3d41480ff5c91dc00aa941ea5af1bebbaaf5d5fd
+    LLVM_HASH=c0d2a71a596d6712451ff28f60b5d6a68276b7da2b4959ca209a3461a0e86f81
     ;;
   *)
     echo "Unsupported arch $ARCH!" 1>&2
@@ -17,5 +17,5 @@ esac
 # Get LLVM
 get_llvm lnx$ARCH $LLVM_HASH
 
-# Get multiarch docker
-
+# Build multiarch docker
+"$DIR/../dockers/multiarch/build.sh"
