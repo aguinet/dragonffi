b5ff12397f01690e06d5b02629d6b929d50d0508
Adrien Guinet <adrien@guinet.me>
2018-12-09 15:09:15 +0100

    Rebase dragonffi on LLVM7.
    Things still to do on the Travis side:
    * have an RTTI-enabled LLVM7 for OSX
    * same for aarch64

--- tests/travis/osx_setup.sh
+++ tests/travis/osx_setup.sh
@@ -4,10 +4,15 @@
 DIR=$(dirname $(realpath $0))
 . "$DIR/common.sh"
 
-get_llvm osx c34f4d9c5634cf93e43ee97ac322702722b5100dfc1c6d7b0a0afb73c6bb5537  
-configure_pip
+#configure_pip
+LLVM_FILE=llvm.tar.xz
+echo "b3ad93c3d69dfd528df9c5bb1a434367babb8f3baea47fbb99bf49f1b03c94ca  $LLVM_FILE" >$LLVM_FILE.sha256
+wget http://releases.llvm.org/7.0.0/clang+llvm-7.0.0-x86_64-apple-darwin.tar.xz -O $LLVM_FILE && shasum -a256 -c $LLVM_FILE.sha256
+mkdir llvm
+tar xf $LLVM_FILE --strip-components 1 -C llvm
+
 
 brew install ccache ninja
-if [ "${PYTHON_VERSION:0:1}" == "3" ]; then
-  brew upgrade python3
-fi
+#if [ "${PYTHON_VERSION:0:1}" == "3" ]; then
+#  brew upgrade python3
+#fi
