d0950c76f91a52b95ab34b2175011764dbdde07b
Adrien Guinet <adrien@guinet.me>
2020-01-29 23:04:47 +0100

    GH actions: linux, osx & windows
    * remove travis
    * proper inclusion of clang libraries (using cmake targets)
    
    TODO:
    * make python tests work on windows
    * make tests work on OSX

--- tests/travis/lnx_py_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-
-DIR=$(dirname $(readlink -f $0))
-. "$DIR/common.sh"
-
-export LLVM_CONFIG=$LNX_LLVM_CONFIG
-cd $DIR/../../bindings/python &&  python ./setup.py build
--- tests/travis/lnx_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-
-DIR=$(dirname $(readlink -f $0))
-. "$DIR/common.sh"
-
-configure_pip
