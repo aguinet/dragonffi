b869caf58d2e8bec2353299c9b2fb3830640103d
Adrien Guinet <aguinet@quarkslab.com>
2020-10-17 19:23:39 +0200

    Python/test_views: make test more portable

--- bindings/python/tests/test_views.py
+++ bindings/python/tests/test_views.py
@@ -23,17 +23,20 @@ class ViewsTest(DFFITest):
     def test_views(self):
         F = self.FFI
         CU = F.cdef('''
+#include <stdint.h>
 typedef struct {
-  unsigned int a;
-  unsigned long long b;
+  uint32_t a;
+  uint64_t b;
 } A;
         ''')
 
-        S = pydffi.view_as(pydffi.const(CU.types.A), b"A"*16)
+        A = CU.types.A
+        Len = A.size
+        S = pydffi.view_as(pydffi.const(A), b"A"*Len)
         self.assertEqual(int(S.a), 0x41414141)
         self.assertEqual(int(S.b), 0x4141414141414141)
         B = bytearray(pydffi.view_as_bytes(S))
-        self.assertEqual(B, b"A"*16)
+        self.assertEqual(B, b"A"*Len)
 
         B = pydffi.view_as_bytes(S)
         One = 1 if sys.version_info >= (3, 0) else struct.pack("B", 1)
