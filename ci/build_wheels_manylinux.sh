b5b8a90690476e97316e6a2f2d47073537a6df74
Adrien Guinet <aguinet@quarkslab.com>
2020-10-17 18:56:36 +0200

    Test Linux Python wheels

--- ci/build_wheels_manylinux.sh
+++ ci/build_wheels_manylinux.sh
@@ -21,7 +21,15 @@ export MAKEFLAGS="-j$(nproc)"
 cd "$DFFI/bindings/python"
 for PY in $*; do
   /opt/python/$PY/bin/python ./setup.py bdist_wheel
+  rm -rf build
 done
 for f in dist/*.whl; do
   auditwheel repair $f
+done
+
+# Tests
+cd "$DFFI"
+for PY in $*; do
+  /opt/python/$PY/bin/pip install bindings/python/wheelhouse/*-$PY-*.whl six purectypes==0.2
+  /opt/python/$PY/bin/python -m unittest discover bindings/python/tests
 done
